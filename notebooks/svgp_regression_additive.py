f0347d9476fb2455c23a17f076eb0213d2cbed10
vincentadam87 <vincent.adam87@gmail.com>
2016-02-18 18:50:03 +0000

    update additive model and demo script

--- notebooks/svgp_regression_additive.py
+++ notebooks/svgp_regression_additive.py
@@ -1,44 +1,44 @@
-from matplotlib import pyplot as plt
 
+from matplotlib import pyplot as plt
 import GPflow.kernels
 from GPflow.likelihoods import Gaussian
 from GPflow.svgp import SVGP
 from GPflow.svgp_additive import SVGP_additive
 import numpy as np
 
-
 np.random.seed(0)
 
-
 # build a very simple data set:
-N = 10
-M = 2
-D = 1
+N = 1000
+M = 20
+D = 2
 X = np.random.rand(N, D)
 Z = X[np.random.permutation(N)[:M]]
 
-s_n = 0.1
-if D == 1:
-    F = np.sin(12 * X) + 0.66 * np.cos(25 * X)
-else:
-    F = np.sin(12 * X[:,0].reshape(N,1)) + 0.66 * np.cos(25 * X[:,0].reshape(N,1))
+f = lambda x : np.sin(12 * x) + 0.66 * np.cos(25 * x)
 
+s_n = 0.1
+F = np.zeros((N,1))
+Fs = np.zeros((N,D))
+if D == 1:
+    F = f(x)
+else:
+    for d in range(D):
+        Fs[:,d] = f(X[:,d])
+    F = np.sum(Fs,axis=1).reshape(N,1)
 Y = F + np.random.randn(N,1) * 0.1
 
-# build the GPR object
-k = GPflow.kernels.Matern52(D)
 likelihood = Gaussian()
-m = SVGP(X, Y, k, likelihood, Z)
 
-k = [GPflow.kernels.Matern52(1) for d in range(D)]
+k = GPflow.kernels.Matern52(1)
 Z = [Z[:,d].reshape(M, 1).copy() for d in range(D)]
 m = SVGP_additive(X, Y, k, likelihood, Z)
-
+m.likelihood.variance = 0.01
 
 m.optimize()
+
 Yp, Vp = m.predict_f(X)
 Sp = np.sqrt(Vp)
-
 
 rmse = np.sqrt(np.mean((Yp - Y) ** 2))
 print "RMSE: %.3f" % rmse
